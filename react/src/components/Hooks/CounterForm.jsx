ef8d7a527a52b9fad23a620943d56e2a4111425e
jnelbaylen <jnelbaylen@gmail.com>
2025-12-30 09:36:23 +0800

    chore: add spacing

--- react/src/components/Hooks/CounterForm.jsx
+++ react/src/components/Hooks/CounterForm.jsx
@@ -1,16 +1,18 @@
-import React from "react";
 import { useIncrementAction } from "@hooks/actionstate/useIncrementAction";
 
-export default function CounterForm() {
+export function CounterForm() {
   const { count, formAction, isPending } = useIncrementAction(0);
 
   return (
     <div style={{ marginBottom: "2rem" }}>
       <h2>Counter Form</h2>
-      <form action={formAction}>
-        <p>Count: {count}</p>
-        <button type="submit" disabled={isPending}>
-          {isPending ? "Incrementing..." : "Increment"}
+      <form action={ formAction }>
+        <p>Count: { count }</p>
+        <button 
+            type="submit" 
+            disabled={ isPending }
+          >
+          { isPending ? "Incrementing..." : "Increment" }
         </button>
       </form>
     </div>
--- react/src/components/Hooks/GreetingForm.jsx
+++ react/src/components/Hooks/GreetingForm.jsx
@@ -1,24 +1,26 @@
-import React from "react";
 import { useGreetingAction } from "@hooks/actionstate/useGreetingAction";
 
-export default function GreetingForm() {
+export function GreetingForm() {
   const { message, formAction, isPending } = useGreetingAction();
 
   return (
     <div>
       <h2>Greeting Form</h2>
-      <form action={formAction}>
+      <form action={ formAction }>
         <input
           type="text"
           name="name"
           placeholder="Enter your name"
           style={{ marginRight: "1rem" }}
         />
-        <button type="submit" disabled={isPending}>
-          {isPending ? "Sending..." : "Greet"}
+        <button 
+          type="submit" 
+          disabled={ isPending }
+        >
+          { isPending ? "Sending..." : "Greet" }
         </button>
       </form>
-      <p>{message}</p>
+      <p>{ message }</p>
     </div>
   );
 }
