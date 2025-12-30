49fef021029722b183bba5157a30148dc18442bd
jnelbaylen <jnelbaylen@gmail.com>
2025-12-30 09:38:24 +0800

    chore: add arrow function and div

--- react/src/pages/About/AboutPage.jsx
+++ react/src/pages/About/AboutPage.jsx
@@ -1,8 +1,8 @@
-export default function AboutPage() {
+export const AboutPage = () => {
     return (
-        <>
+        <div>
             <h1>About</h1>
             <p>Hello there. <br />How do you do?</p> 
-        </>
+        </div>
     )
 }
