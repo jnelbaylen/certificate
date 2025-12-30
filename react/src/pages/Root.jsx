31cc6ad0abc0fa1d405b9879251706176bd9c57f
jnelbaylen <jnelbaylen@gmail.com>
2025-12-30 09:30:37 +0800

    chore: update name imports

--- react/src/pages/Root.jsx
+++ react/src/pages/Root.jsx
@@ -1,26 +1,28 @@
-import MyButton from "@components/Button/MyButton";
-import MyProfile from "@components/Profile/MyProfile";
-import CounterForm from "@components/Hooks/CounterForm";
-import GreetingForm from "@components/Hooks/GreetingForm";
+import { Fragment } from "react";
 
-import AboutPage from "./About/AboutPage";
-import ShoppingList from "./Shop/ShoppingList";
+import { Button } from "@components/Button/Button";
+import { Avatar } from "@components/Profile/Avatar";
+import { CounterForm } from "@components/Hooks/CounterForm";
+import { GreetingForm } from "@components/Hooks/GreetingForm";
+
+import { AboutPage } from "./About/AboutPage";
+import { ShoppingList } from "./Shop/ShoppingList";
 
 function Greeting({ name }) {
-  return <h1>Hello, {name}</h1>
+  return <h1>Hello, { name }</h1>
 }
 
-export default function Root() {
+export function Root() {
   return (
-    <>
+    <Fragment>
       <h1>Welcome to app</h1>
-      <MyButton />
+      <Button />
       <AboutPage />
       <Greeting name="World" />
-      <MyProfile />
+      <Avatar />
       <ShoppingList />
       <CounterForm />
       <GreetingForm />
-    </>
+    </Fragment>
   )
 }
