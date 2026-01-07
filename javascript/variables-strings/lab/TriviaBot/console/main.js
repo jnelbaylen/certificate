6e4ce7de8d157662a57fe23e6695aa07dfad2bf6
jnelbaylen <jnelbaylen@gmail.com>
2026-01-07 22:16:42 +0800

    feat: add lab for trivia bot

---
+++ javascript/variables-strings/lab/TriviaBot/console/main.js
@@ -0,0 +1,24 @@
+let greetingMessage = "Hello! I'm your coding fun fact guide!"
+
+console.log( greetingMessage );
+
+let botName = "botName";
+let botLocation = "botLocation";
+let favoriteLanguage = "favoriteLanguage";
+
+console.log(`My name is ${ botName } and I live on ${ botLocation }.`);
+console.log(`My favorite programming language is ${ favoriteLanguage }.`);
+
+let codingFact = favoriteLanguage;
+
+console.log( codingFact );
+
+codingFact = "My " + favoriteLanguage + " is javascript.";
+
+console.log( codingFact );
+
+codingFact = "My favorite programming language is " + favoriteLanguage + ".";
+
+console.log( codingFact );
+
+console.log(`It was fun sharing these facts with you. Goodbye! - ${ botName } from ${ botLocation }.`);
