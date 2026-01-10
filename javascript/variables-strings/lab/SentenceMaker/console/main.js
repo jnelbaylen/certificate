16378539d534f6080817b60ba9ee819550e58b27
jnelbaylen <jnelbaylen@gmail.com>
2026-01-10 16:17:00 +0800

    feat: add sentence maker console

---
+++ javascript/variables-strings/lab/SentenceMaker/console/main.js
@@ -0,0 +1,24 @@
+let adjective = "adjective";
+let noun = "noun";
+let verb = "verb";
+let place = "place";
+let adjective2 = "adjective2";
+let noun2 = "noun2";
+
+let firstStory;
+firstStory = `Once upon a time, there was a(n) ${ adjective } ${ noun} who loved to eat ${ noun2 }. The ${ noun } lived in a ${ place } and had ${ adjective2 } nostrils that blew fire when it was ${ verb }.`;
+
+console.log("First story: " + firstStory);
+
+let secondStory;
+
+adjective = "sleepy";
+noun = "unicorn";
+verb = "snoring";
+place = "meadow";
+adjective2 = "sparkly";
+noun2 = "cupcakes";
+
+secondStory = `Once upon a time, there was a(n) ${ adjective } ${ noun } who loved to eat ${ noun2 }. The ${ noun } lived in a ${ place } and had ${ adjective2 } nostrils that blew fire when it was ${ verb }.`
+
+console.log("Second story: " + secondStory);
