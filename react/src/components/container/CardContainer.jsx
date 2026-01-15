da3c3584a42d0c3d7cc56dbded76388f0cec9a85
jnelbaylen <jnelbaylen@gmail.com>
2026-01-15 16:39:08 +0800

    feat: add container
    - card container
    - card template

---
+++ react/src/components/container/CardContainer.jsx
@@ -0,0 +1,34 @@
+import { Card } from "./CardTemplate"
+
+import { PROFILES } from "@lib/constants"
+
+export function CardContainer(){
+    return(
+        <div className="flex-container">
+            {/* <Card 
+                name="Mark"
+                title="Frontend developer"
+                bio="I like to work with different frontend technologies and play video games."
+            />
+            <Card 
+                name="Tiffany"
+                title="Engineering manager"
+                bio="I have worked in tech for 15 years and love to help people grow in this industry."
+            />
+            <Card 
+                name="Doug"
+                title="Backend developer"
+                bio="I have been a software developer for over 20 years and I love working with Go and Rust."
+            /> */}
+
+            { PROFILES.map((profile) => (
+                <Card 
+                    key={ profile.id }
+                    name={ profile.name }
+                    title={ profile.title }
+                    bio={ profile.bio }
+                />
+            ))}
+        </div>
+    )
+}
---
+++ react/src/components/container/CardTemplate.jsx
@@ -0,0 +1,10 @@
+export const Card = ({ name, title, bio }) => {
+    return(
+        <div className="card">
+            <h2> { name } </h2>
+            <p className="card-title">{ title }</p>
+            <p>{ bio }</p>
+        </div>
+    )
+}
+
