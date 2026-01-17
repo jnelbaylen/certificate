d52a1be68d1a2aef071fd69604d144b8716e2566
jnelbaylen <jnelbaylen@gmail.com>
2026-01-17 14:43:18 +0800

    feat: add fruits search

--- react/src/pages/Root.jsx
+++ react/src/pages/Root.jsx
@@ -2,12 +2,14 @@ import { Fragment } from "react"
 
 import { Navbar } from "@components/Navbar"
 import { CardContainer } from "@components/container/CardContainer"
+import { FruitsSearch } from "@components/FruitsSearch"
 
 export function Root() {
   return (
    <Fragment>
       <Navbar />
       <CardContainer />
+      <FruitsSearch />
    </Fragment>
   )
 }
