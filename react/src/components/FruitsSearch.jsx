8b23356ed90ed10ef500dce1e2eec56ff8bbb8c1
jnelbaylen <jnelbaylen@gmail.com>
2026-01-17 14:42:53 +0800

    feat: add results and function

--- react/src/components/FruitsSearch.jsx
+++ react/src/components/FruitsSearch.jsx
@@ -1,16 +1,38 @@
-import { useState } from "react"
+import { useCallback, useState } from "react"
 
 export function FruitsSearch() {
 
     const [ query, setQuery ] = useState('')
     const [ results, setResults ] = useState([])
 
+    const onHandleChange = useCallback((event) => {
+        setQuery(event.currentTarget.value)
+    })
+
+    const onHandleSubmit = useCallback((event) => {
+        event.preventDefault()
+    })
+
     return(
         <div id="search-container">
-            <form>
+            <form onSubmit={ onHandleSubmit }>
                 <label htmlFor="search-input">Search for fruits:</label>
-                <input id="search-input" type="search"  />
+                <input 
+                    id="search-input" 
+                    type="search"
+                    value={ query }  
+                    onChange={ onHandleChange }
+                />
             </form>
+
+            <div id="results">
+                { results.length > 0 ? ( results.map(item => ( 
+                    <p key={ item } className="results-item">{ item }</p> 
+                    )) 
+                ) : (
+                    <p>No results found</p>
+                )}
+            </div>
         </div>
     )
 }
